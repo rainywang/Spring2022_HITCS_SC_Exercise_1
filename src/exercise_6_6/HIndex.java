69d20023ccb4e1064ed95a26f753898d63303684
Wang Zhongjie <rainy.wang@gmail.com>
2020-01-30 14:39:33 +0800

    v0.4 avoid input string containing illegal characters

--- src/exercise_6_6/HIndex.java
+++ src/exercise_6_6/HIndex.java
@@ -10,27 +10,42 @@ public class HIndex {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Please input the citation numbers:");
 		
-		//deal with empty input
-		String line = new String();
-		line = scanner.nextLine();
-		while(line.length() == 0) {
-			System.out.println("Input empty, please re-input:");
-			line = scanner.nextLine();
-		}
-
-		//split input string into integers
 		int[] citations = new int[100];
 		String[] strs;
+		String line = new String();
 		
-		strs = line.split(",");
-		for (int i = 0; i < strs.length; i++) 
-			citations[i] = Integer.parseInt(strs[i]);
-
-		//calculate h-index
-		int hindex = hindex(citations);
-		
-		//output to console
-		System.out.println("The h-index is: " + hindex);
+		//loop, until user inputs legal string
+		while (true) {
+			line = scanner.nextLine();
+			//if the input is empty
+			if (line.length() == 0) {
+				System.out.println("Input empty, please re-input:");
+				continue;
+			}
+			
+			//check if each part is integer >= 0
+			boolean legalNumbers = true;
+			strs = line.split(",");
+			for (int i = 0; i < strs.length; i++) {
+				//if not, stop checking others and let user re-input
+				if(! strs[i].matches("[0-9]+")) {
+					System.out.println(strs[i] + " is illegal, please input again: ");
+					legalNumbers = false;
+					break;
+				}
+				//otherwise, store the integer into array
+				citations[i] = Integer.parseInt(strs[i]); 
+			}
+			if (!legalNumbers)
+				continue;
+			else {
+				//calculate h-index
+				int hindex = hindex(citations);
+				//output to console
+				System.out.println("The h-index is: " + hindex);
+				break;
+			}
+		}
 	}
 	
 	public static int hindex(int[] citations) {
